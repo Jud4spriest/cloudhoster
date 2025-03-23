5b9949eb8d364b9c614f3b7490bd160bb060c4d2
Marcos Azevedo <mazevedo.snts@gmail.com>
2025-03-23 08:19:48 -0300

    [Fix] Removed custom user and entrypoint from Dockerfile. Fixed permissions

--- cloudhoster/scripts/start.sh
+++ cloudhoster/scripts/start.sh
@@ -1,4 +1,19 @@
 #!/bin/bash
+
+# ----------------------
+echo "[Debug] User executing this script: $(whoami)"
+
+if [ -f /.dockerenv ] || grep -qa 'docker' /proc/1/cgroup; then
+    echo "[Debug] Running inside the container"
+else
+    echo "[Debug] Running outside the container (host machine)"
+fi
+# ----------------------
+
+echo "Fixing container's user permissions..."
+chown -R www-data:www-data /var/www/html
+chmod -R 750 /var/www/html
+
 echo "Waiting for MariaDB to be ready..."
 until mysqladmin ping -h mariadb --silent; do
     sleep 2
@@ -6,14 +21,11 @@ done
 echo "MariaDB is up! Starting services..."
 
 bash /config/roundcube-init.sh
-sudo /usr/sbin/php-fpm8.2 -D
-
-echo "Fixing permissions..."
-chown -R appuser:www-data /var/www/html
-chmod -R 775 /var/www/html
-
+echo "initializing php-fpm ..."
+/usr/sbin/php-fpm8.2 -D
 echo "php-fpm initialized!"
-sudo -u appuser /usr/sbin/vsftpd &
-sudo -u appuser /usr/sbin/nginx -g "daemon off;"
+
+sudo -u www-data /usr/sbin/vsftpd &
+sudo -u www-data /usr/sbin/nginx -g "daemon off;"
 
 #tail -f /dev/null
